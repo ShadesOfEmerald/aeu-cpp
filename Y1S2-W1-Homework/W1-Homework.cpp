574264621f639c49423f58b52eec152586a9fd88
[ShadesOfEmerald] <[grapleen@gmail.com]>
2024-07-28 00:18:32 +0700

    Y1S2-Homework

--- Y1S2-W1-Homework/W1-Homework.cpp
+++ Y1S2-W1-Homework/W1-Homework.cpp
@@ -1,7 +1,7 @@
 //Fri, Jul 19
 //Name: Sing Soneth
 //ID:   64725
-//Y1S2 - IT Associate - Evening Class
+//Y1S2.2 - IT Ass. - Evening Class
 
 #include <iostream>
 using namespace std;
@@ -9,7 +9,12 @@ using namespace std;
 int main(){
     int n, a=1, b=0;
     start:
-    cout << "Input n = "; cin >> n;
+    cout << "Input n = "; cin >> noskipws >> n;
+    while (cin.fail()){
+        cin.clear();
+	    cin.ignore(256, '\n');
+        goto start;
+    }
     if (n>=1 && n<=10){
         cout << "------------\n";
         do {
---
+++ Y1S2-W2-Homework/W2-Homework.cpp
@@ -0,0 +1,43 @@
+//Fri, Jul 27
+//Name:     Sing Soneth
+//ID:       64725
+//Year:     Y1S2.G2
+//Major:    IT Associate
+//Time:     Evening Class
+
+#include <iostream>
+using namespace std;
+
+void block() {
+    int count, total=1, evenCheck;
+    start:
+    cout << "Input: ";
+    cin >> count;
+
+    if(count<1 || count>10) goto start;
+    cout << "===\n\n";
+
+    for(int i=1; i<=count; i++) {
+        if(i<count){
+            evenCheck = i % 2;
+            if (evenCheck==0) continue;
+            cout << i << " * ";
+            total = total * i;
+            
+        } else {
+            evenCheck = i % 2;
+            if (evenCheck==0){
+                cout << " = " << total << "\n";
+            } else {
+            total = total * i;
+            cout << i << " = " << total << "\n";}
+        }
+    }
+
+    system("pause");
+}
+
+int main(){
+    block();
+    return 0;
+}
