cd3331a150d06b9e7a95c07a02d251f98a9621b0
ShadesOfEmerald-ipad <grapleen@gmail.com>
2024-08-21 09:57:36 +0000

    W5 changes

--- Y1S2-W5-Excercise/excercize-w5.cpp
+++ Y1S2-W5-Excercise/excercize-w5.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 void input(double &x, double &y);
-void output(double &x, double &y);
-double area(double &x, double &y);
-double perim(double &x, double &y);
+void output(double x, double y);
+double area(double x, double y);
+double perim(double x, double y);
 
 int main(){
     double width, length;
@@ -21,23 +21,23 @@ void input(double &x, double &y){
     cout << "======================" << endl;
 }
 
-void output(double &x, double &y){
+void output(double x, double y){
     cout << "\nResult:\n";
     cout << "======================\n";
     cout << "Rectangle Width: " << x << "\n";
     cout << "Rectangle Length: " << y << "\n";
     cout << "Rectangle Area: " << area(x, y) << "\n";
     cout << "Rectangle Perimeter: " << perim(x, y) << "\n";
-    cout << "\nwomp womp \n";
+    cout << "======================\n";
 }
 
-double area(double &x, double &y){
+double area(double x, double y){
     double answer;
     answer = x*y;
     return answer;
 }
 
-double perim(double &x, double &y){
+double perim(double x, double y){
     double answer;
     answer = 2*(x+y);
     return answer;
